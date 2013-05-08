5dfe0c355c660ae9a678ce7671918cbb54134d1e
Sergio Fernández Granados <sergio@yoprogramador.com>
2013-05-08 12:05:00 +0200

    Commit inicial

---
+++ src/com/universogtp/fillthejar/JarRules.java
@@ -0,0 +1,15 @@
+package com.universogtp.fillthejar;
+
+public class JarRules {
+	public JarRules(){
+		
+	}
+
+	public int getCounter() {
+		return 35;
+	}
+	
+	public int fillJar() {
+		return 36;
+	}	
+}
