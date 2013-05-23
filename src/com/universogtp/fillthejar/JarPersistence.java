c223133f79bf1a08d12fa2b2c37aa3f7728ffb5e
jorgeleon79 <jorgeleon79@gmail.com>
2013-05-23 18:56:03 +0200

    add fields to the database

--- src/com/universogtp/fillthejar/JarPersistence.java
+++ src/com/universogtp/fillthejar/JarPersistence.java
@@ -8,8 +8,15 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class JarPersistence {
 	public static final String ID_ROW = "_id";
-	public static final String REASON = "name";
+	public static final String NAME = "name";
 	public static final String VALUE = "value";
+	public static final String CREATED = "date_of_created"; // poner fecha actual
+	public static final String FRECUENCY = "frecuency";
+	public static final String WEEKENDS = "weekends";
+	public static final String FILLPERCYCLE = "fill_per_cycle";
+	public static final String FILLTHISCYCLE = "fill_this_cycle";
+	public static final String LASTFILL = "last_fill";
+	public static final String STREAK = "streak";
 
 	
 	private static final String N_DB = "fill_the_jar";
@@ -28,7 +35,8 @@ public class JarPersistence {
 		public void onCreate(SQLiteDatabase db) {
 			db.execSQL("CREATE TABLE "+N_TABLE+ "("
 					+ ID_ROW+" INTEGER PRIMARY KEY AUTOINCREMENT, "
-					+ REASON+" TEXT NOT NULL, "
+					+ NAME+" TEXT NOT NULL, "
+					+ CREATED+" INTEGER, "
 					+VALUE+" INTEGER NOT NULL);");
 		}
 
@@ -61,7 +69,7 @@ public class JarPersistence {
 	
 	public void newJar(Jar jar) {
 		ContentValues values = new ContentValues();
-		values.put(REASON,jar.getName());
+		values.put(NAME,jar.getName());
 		values.put(VALUE,jar.getValue());
 		
 		long id = db.insert(N_TABLE, null, values);
@@ -70,7 +78,7 @@ public class JarPersistence {
 	
 	public void updateJar(Jar jar){
 		ContentValues values = new ContentValues();
-		values.put(REASON,jar.getName());
+		values.put(NAME,jar.getName());
 		values.put(VALUE,jar.getValue());
 
 		db.update(N_TABLE, values, ID_ROW+"='"+jar.getID()+"'", null);
@@ -83,7 +91,7 @@ public class JarPersistence {
 	public JarList getJarList()  throws Exception {
 		JarList jarList = new JarList();
 		
-		String[] columns = new String[]{ID_ROW, REASON, VALUE};
+		String[] columns = new String[]{ID_ROW, NAME, VALUE};
 		Cursor cursor = null;
 		cursor = db.query(N_TABLE, columns, null, null, null, null, null);
 		int numRows = cursor.getCount();
@@ -91,10 +99,10 @@ public class JarPersistence {
 		
 		for (int i = 0; i < numRows; ++i) {
 			long id = cursor.getLong(0);
-			String reason = cursor.getString(1);
+			String NAME = cursor.getString(1);
 			int value = cursor.getInt(2);
 			
-			Jar jar = new Jar(id, reason);
+			Jar jar = new Jar(id, NAME);
 			jar.setValue(value);
 			
 			jarList.addJar(jar);
